61ef6b2a85b2be3eda4f14eda79b7f65e3fe30e0
computer science <kasia.cabon@gmail.com>
2019-12-14 13:32:00 +0100

    load to file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,41 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.imageio.stream.FileImageInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main extends Application {
+    int[][] array = new int[1920][1080];
+
+    public void loadFile() throws FileNotFoundException {
+        Image image = new Image(new FileInputStream("ABC.png"));
+
+        PixelReader pixelReader = image.getPixelReader();
+        for (int y = 0; y < 1080; y++) {
+            for (int x = 0; x < 1920; x++) {
+                Color color = pixelReader.getColor(x, y);
+                if (color == Color.BLACK) {
+                    array[x][y]=1;
+                } else{ array[x][y]=0;
+
+                }
+
+
+            }
+        }
+        System.out.println("works");
+    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        loadFile();
     }
 
 
